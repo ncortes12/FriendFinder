b29a111b7919904755986c6029a9294dac4d9c36
ncortes12 <ncortes1415@gmail.com>
2018-07-29 16:26:36 -0700

    change routes in server

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ var PORT = process.env.PORT || 8080;
 app.use(express.static("public"))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-require("./routing/apiRoutes.js")(app);
-require("./routing/htmlRoutes.js")(app);
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
 
 app.listen(PORT, function (){
     console.log("App listening on PORT " + PORT);
